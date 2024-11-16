7d53137df9313a18e6d197aa9043cfcbeada9e26
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2024-11-16 14:22:11 -0300

    Add next-auth functionality to NavBar component

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,12 +1,35 @@
+import { auth, signIn, signOut } from "@/auth";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 async function NavBar() {
+  const session = await auth();
+
   return (
     <header>
       <nav>
         <Link href="/"><Image alt="YC Clone" src="/logo.png" width={64} height={64} unoptimized={true}/></Link>
+        {session && session?.user ? (
+          <div>
+            <Link href="/startup/create">Create</Link>
+            <form action={async () => {
+              "use server";
+              await signOut();
+            }}>
+              <button type="submit">Log Out</button>
+            </form>
+            <Link href={`/users/${session?.user.id}`}>
+              <span>{session?.user.name}</span>
+            </Link>
+          </div>
+        ) : <form action={async () => {
+          "use server";
+          await signIn('github');
+        }}>
+          <button type="submit">Log In</button>
+        </form>
+        }
       </nav>
     </header>
   )
