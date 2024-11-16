3954586728496320f3cdb956603f17f497304744
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2024-11-16 15:50:10 -0300

    Convert required imports into default imports

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+import cssanimate from "tailwindcss-animate";
+import typography from "@tailwindcss/typography";
 
 const config: Config = {
   darkMode: ["class"],
@@ -45,7 +47,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [cssanimate, typography],
 };
 
 export default config;
